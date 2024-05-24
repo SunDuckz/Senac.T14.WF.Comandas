ed5cede42512b5ad150c1c87ac0fcfd8a1c4b1c7
SunDuckz <gui.bampi0@gmail.com>
2024-05-23 21:47:49 -0300

    Criacao do projeto Comandas

---
+++ Comandas/FrmPrincipal.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Comandas
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gpbPrincipal = new GroupBox();
+            btnCardapio = new ReaLTaiizor.Controls.CyberButton();
+            btnComanda = new ReaLTaiizor.Controls.CyberButton();
+            btnPedidoCozinha = new ReaLTaiizor.Controls.CyberButton();
+            btnUsuarios = new ReaLTaiizor.Controls.CyberButton();
+            gpbPrincipal.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gpbPrincipal
+            // 
+            gpbPrincipal.Controls.Add(btnUsuarios);
+            gpbPrincipal.Controls.Add(btnPedidoCozinha);
+            gpbPrincipal.Controls.Add(btnComanda);
+            gpbPrincipal.Controls.Add(btnCardapio);
+            gpbPrincipal.Location = new Point(38, 109);
+            gpbPrincipal.Name = "gpbPrincipal";
+            gpbPrincipal.Size = new Size(717, 306);
+            gpbPrincipal.TabIndex = 0;
+            gpbPrincipal.TabStop = false;
+            // 
+            // btnCardapio
+            // 
+            btnCardapio.Alpha = 20;
+            btnCardapio.BackColor = Color.Transparent;
+            btnCardapio.Background = true;
+            btnCardapio.Background_WidthPen = 4F;
+            btnCardapio.BackgroundPen = true;
+            btnCardapio.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnCardapio.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnCardapio.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnCardapio.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnCardapio.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnCardapio.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnCardapio.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnCardapio.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnCardapio.Effect_1 = true;
+            btnCardapio.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnCardapio.Effect_1_Transparency = 25;
+            btnCardapio.Effect_2 = true;
+            btnCardapio.Effect_2_ColorBackground = Color.White;
+            btnCardapio.Effect_2_Transparency = 20;
+            btnCardapio.Font = new Font("Arial", 11F);
+            btnCardapio.ForeColor = Color.FromArgb(245, 245, 245);
+            btnCardapio.Lighting = false;
+            btnCardapio.LinearGradient_Background = false;
+            btnCardapio.LinearGradientPen = false;
+            btnCardapio.Location = new Point(22, 101);
+            btnCardapio.Name = "btnCardapio";
+            btnCardapio.PenWidth = 15;
+            btnCardapio.Rounding = true;
+            btnCardapio.RoundingInt = 70;
+            btnCardapio.Size = new Size(133, 110);
+            btnCardapio.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnCardapio.TabIndex = 0;
+            btnCardapio.Tag = "Cyber";
+            btnCardapio.TextButton = "Cardapio";
+            btnCardapio.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnCardapio.Timer_Effect_1 = 5;
+            btnCardapio.Timer_RGB = 300;
+            // 
+            // btnComanda
+            // 
+            btnComanda.Alpha = 20;
+            btnComanda.BackColor = Color.Transparent;
+            btnComanda.Background = true;
+            btnComanda.Background_WidthPen = 4F;
+            btnComanda.BackgroundPen = true;
+            btnComanda.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnComanda.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnComanda.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnComanda.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnComanda.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnComanda.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnComanda.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnComanda.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnComanda.Effect_1 = true;
+            btnComanda.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnComanda.Effect_1_Transparency = 25;
+            btnComanda.Effect_2 = true;
+            btnComanda.Effect_2_ColorBackground = Color.White;
+            btnComanda.Effect_2_Transparency = 20;
+            btnComanda.Font = new Font("Arial", 11F);
+            btnComanda.ForeColor = Color.FromArgb(245, 245, 245);
+            btnComanda.Lighting = false;
+            btnComanda.LinearGradient_Background = false;
+            btnComanda.LinearGradientPen = false;
+            btnComanda.Location = new Point(211, 101);
+            btnComanda.Name = "btnComanda";
+            btnComanda.PenWidth = 15;
+            btnComanda.Rounding = true;
+            btnComanda.RoundingInt = 70;
+            btnComanda.Size = new Size(133, 110);
+            btnComanda.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnComanda.TabIndex = 1;
+            btnComanda.Tag = "Cyber";
+            btnComanda.TextButton = "Comanda";
+            btnComanda.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnComanda.Timer_Effect_1 = 5;
+            btnComanda.Timer_RGB = 300;
+            // 
+            // btnPedidoCozinha
+            // 
+            btnPedidoCozinha.Alpha = 20;
+            btnPedidoCozinha.BackColor = Color.Transparent;
+            btnPedidoCozinha.Background = true;
+            btnPedidoCozinha.Background_WidthPen = 4F;
+            btnPedidoCozinha.BackgroundPen = true;
+            btnPedidoCozinha.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnPedidoCozinha.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnPedidoCozinha.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnPedidoCozinha.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnPedidoCozinha.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnPedidoCozinha.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnPedidoCozinha.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnPedidoCozinha.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnPedidoCozinha.Effect_1 = true;
+            btnPedidoCozinha.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnPedidoCozinha.Effect_1_Transparency = 25;
+            btnPedidoCozinha.Effect_2 = true;
+            btnPedidoCozinha.Effect_2_ColorBackground = Color.White;
+            btnPedidoCozinha.Effect_2_Transparency = 20;
+            btnPedidoCozinha.Font = new Font("Arial", 11F);
+            btnPedidoCozinha.ForeColor = Color.FromArgb(245, 245, 245);
+            btnPedidoCozinha.Lighting = false;
+            btnPedidoCozinha.LinearGradient_Background = false;
+            btnPedidoCozinha.LinearGradientPen = false;
+            btnPedidoCozinha.Location = new Point(395, 101);
+            btnPedidoCozinha.Name = "btnPedidoCozinha";
+            btnPedidoCozinha.PenWidth = 15;
+            btnPedidoCozinha.Rounding = true;
+            btnPedidoCozinha.RoundingInt = 70;
+            btnPedidoCozinha.Size = new Size(133, 110);
+            btnPedidoCozinha.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnPedidoCozinha.TabIndex = 2;
+            btnPedidoCozinha.Tag = "Cyber";
+            btnPedidoCozinha.TextButton = "Pedido Cozinha";
+            btnPedidoCozinha.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnPedidoCozinha.Timer_Effect_1 = 5;
+            btnPedidoCozinha.Timer_RGB = 300;
+            // 
+            // btnUsuarios
+            // 
+            btnUsuarios.Alpha = 20;
+            btnUsuarios.BackColor = Color.Transparent;
+            btnUsuarios.Background = true;
+            btnUsuarios.Background_WidthPen = 4F;
+            btnUsuarios.BackgroundPen = true;
+            btnUsuarios.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnUsuarios.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnUsuarios.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnUsuarios.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnUsuarios.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnUsuarios.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnUsuarios.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnUsuarios.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnUsuarios.Effect_1 = true;
+            btnUsuarios.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnUsuarios.Effect_1_Transparency = 25;
+            btnUsuarios.Effect_2 = true;
+            btnUsuarios.Effect_2_ColorBackground = Color.White;
+            btnUsuarios.Effect_2_Transparency = 20;
+            btnUsuarios.Font = new Font("Arial", 11F);
+            btnUsuarios.ForeColor = Color.FromArgb(245, 245, 245);
+            btnUsuarios.Lighting = false;
+            btnUsuarios.LinearGradient_Background = false;
+            btnUsuarios.LinearGradientPen = false;
+            btnUsuarios.Location = new Point(578, 101);
+            btnUsuarios.Name = "btnUsuarios";
+            btnUsuarios.PenWidth = 15;
+            btnUsuarios.Rounding = true;
+            btnUsuarios.RoundingInt = 70;
+            btnUsuarios.Size = new Size(133, 110);
+            btnUsuarios.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnUsuarios.TabIndex = 3;
+            btnUsuarios.Tag = "Cyber";
+            btnUsuarios.TextButton = "Usuarios";
+            btnUsuarios.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnUsuarios.Timer_Effect_1 = 5;
+            btnUsuarios.Timer_RGB = 300;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(gpbPrincipal);
+            Name = "FrmPrincipal";
+            Text = "Form1";
+            gpbPrincipal.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox gpbPrincipal;
+        private ReaLTaiizor.Controls.CyberButton btnUsuarios;
+        private ReaLTaiizor.Controls.CyberButton btnPedidoCozinha;
+        private ReaLTaiizor.Controls.CyberButton btnComanda;
+        private ReaLTaiizor.Controls.CyberButton btnCardapio;
+    }
+}
